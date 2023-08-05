6f077fde750cb218720c1380b631ac4d5b38063a
David Hospital <david.hospital27@gmail.com>
2023-08-05 12:14:54 -0400

    add <C-j> and <C-k> keymaps for cycling between buffers

--- lua/remaps.lua
+++ lua/remaps.lua
@@ -1,3 +1,8 @@
 vim.g.mapleader = " "
 
 vim.keymap.set("n", "<leader>d", vim.cmd.Ex)
+
+
+-- buffer keymaps
+vim.keymap.set("n", "<C-j>", "<Cmd>bnext<CR>")
+vim.keymap.set("n", "<C-k>", "<Cmd>bprev<CR>")
